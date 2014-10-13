c381b63e9c5dd5238cc9e4da462af1bac544cd91
yunixon <yunixon@gmail.com>
2014-10-13 14:45:37 +0400

    body validations

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,10 @@
 class Resume < ActiveRecord::Base
+  enum status: {unpublished: 0, published: 1}
+
   belongs_to :user
+  
+  validates :body, presence: true
+  validates :body, length: {minimum: 3, maximum: 400}
 
   validates_associated :user
 
