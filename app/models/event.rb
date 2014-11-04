5f1d2c5006c25beed49b81958732340e946f5ef4
yunixon <yunixon@gmail.com>
2014-11-04 16:28:25 +0400

    fix event friendlyid, show event owner

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,17 @@
+require "babosa"
+
 class Event < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
 
   belongs_to :organizers, class_name: "User"
+
+  def owner
+    User.find_by id: self.organizer_id
+  end
+
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
 
 end
