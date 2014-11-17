c27cae2bc910e073905a09e76b3ef34e1b47820f
yunixon <yunixon@gmail.com>
2014-11-17 07:27:50 +0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['Вечеринка Ростелеком', 'Встреча рубистов', 'Встреча сисадминов', 'Учеба 1С', 'Учеба виртуализации', 'Учеба HyperV',
+  'Microsoft Windows сертификация', 'Соревнование сисадминов', 'Выиграй книгу'].each do |title|
+  Event.find_or_create_by({title: title})
+end
+
+['Ростелеком работа', 'Работа', 'Работа для сисадминов', '1с работа', 'Подработка эникею', 'Настройка виртуализации',
+  'Помощь в получении сертификата', 'Удаленная настройка сервера', 'Написание скрипта'].each do |name|
+  Job.find_or_create_by({name: name})
+end
