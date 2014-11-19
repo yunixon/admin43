02234123347d9d6513b066069acce28b655c8b1e
yunixon <yunixon@gmail.com>
2014-11-19 16:47:54 +0400

    add status to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119062024) do
+ActiveRecord::Schema.define(version: 20141119124557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141119062024) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.string   "status"
   end
 
   add_index "events", ["slug"], name: "index_events_on_slug", unique: true, using: :btree
