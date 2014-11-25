0676e2860a00323f06c1afffbcd3a322fb3d06e8
yunixon <yunixon@gmail.com>
2014-11-25 10:19:45 +0400

    user show refactoring and add description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :photo)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :photo, :description)
   end
 
 end
