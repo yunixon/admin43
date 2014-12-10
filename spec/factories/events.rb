090208b2c24c68a5d6ef0d3a510bf5c8a129cea3
yunixon <yunixon@gmail.com>
2014-12-10 18:31:25 +0400

    event model spec and factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,13 +1,15 @@
 FactoryGirl.define do
+  
   factory :event do
-    title 'MyString'
+    title 'My Event'
     start_date '2014-11-03 10:45:11'
     end_date '2014-11-03 10:45:11'
-    location 'MyString'
-    agenda 'MyText'
-    address 'MyText'
-    logo 'MyString'
+    location 'Kirov'
+    agenda 'Povestka'
+    address 'Moskovskaya 4'
+    logo 'url of logo'
     organizer_id 1
+    status 'published'
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  
+  it 'is valid with an all columns' do
+    expect(build(:event)).to be_valid
+  end
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :start_date }
+  it { should validate_presence_of :end_date }
+  it { should validate_presence_of :location }
+  it { should validate_presence_of :agenda }
+  it { should validate_presence_of :address }
+  
+  it 'return event`s owner' do
+  end
+
 end
