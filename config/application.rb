b477916fd3186fcb11b7f853ce77541b1f707ea6
yunixon <yunixon@gmail.com>
2014-12-10 16:53:21 +0400

    rspec config, generators, delete unneeded tests

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,17 @@ module Admin43
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('lib', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+    
   end
 end
